3306df563a9955a8cd848e0608b56f7bad381c00
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-20 15:07:37 +0100

    update

--- lib/app.rb
+++ lib/app.rb
@@ -3,6 +3,26 @@ require 'dotenv'
 
 Dotenv.load('../.env')
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
 def login_twitter
   client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
@@ -10,13 +30,6 @@ def login_twitter
     config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
     config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
   end
-  client.update('Mon premier tweet en Ruby!!!!')
+  return client.update('Mon premier tweet en Ruby!!!!')
 end
-
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-end
-client.update('My first ruby tweet!')
+=end
